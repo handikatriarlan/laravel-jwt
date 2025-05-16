40d570b6bb78f4e5a4dd8657bce5604174ee4150
handikatriarlan <handikaarlan@gmail.com>
2025-05-16 10:43:04 +0700

    feat(logout): add jwt exception handling in logout controller

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class LogoutController extends Controller
 {
