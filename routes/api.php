800289c1dac849a161f57569d49d4923ffdc8ff7
handikatriarlan <handikaarlan@gmail.com>
2025-05-16 10:37:46 +0700

    feat: implement login endpoint and get user data

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 
-Route::get('/user', function (Request $request) {
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
